0fa2b3d671041be7052cb7c95923d65c504d591e
rawlesson <35452403+rawlesson@users.noreply.github.com>
2018-01-15 17:59:58 +0700

    Update bot_test.py

--- bot_test.py
+++ bot_test.py
@@ -5,7 +5,7 @@ import datetime
 class BotHandler:
 
     def __init__(self, token):
-        self.token = tokenhhh
+        self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
     def get_updates(self, offset=None, timeout=30):
